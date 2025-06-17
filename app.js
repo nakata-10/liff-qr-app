d2d18864d411adbfe8c826a5b6f504dd64349341
nakata-10 <tsugumi2528@gmail.com>
2025-06-17 13:15:36 +0900

    Use absolute URL for scan.html in QR code

--- app.js
+++ app.js
@@ -19,14 +19,8 @@ function setupQrGeneration() {
     document.getElementById('qr-result').textContent = '';
 
     const codeText = Date.now().toString();
-    // 【相対パス版】
-//  const link = `scan.html?code=${codeText}`;
-
-    // 【絶対URL版】（必要に応じてこちらを使ってください）
-//  const link = `https://nakata-10.github.io/liff-qr-app/scan.html?code=${codeText}`;
-
-    // 推奨：相対パス
-    const link = `scan.html?code=${codeText}`;
+    // ここを絶対URLに変更！
+    const link = `https://nakata-10.github.io/liff-qr-app/scan.html?code=${codeText}`;
 
     new QRCode(document.getElementById('qrcode'), {
       text: link,
@@ -35,6 +29,7 @@ function setupQrGeneration() {
     });
   });
 }
+
 
 initializeLiff().then(() => {
   setupQrGeneration();
