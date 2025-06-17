8bfeca6e1b2d5ec94c7e0c5b35bdc288a385985e
nakata-10 <tsugumi2528@gmail.com>
2025-06-17 14:37:34 +0900

    Add debug logs to scan.html

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // public/config.js
 window.APP_CONFIG = {
   LIFF_ID: "2007532568-4ggdRvjk",
-  AZURE_FUNCTION_URL: "https://…/api/awardPoints?code=<YOUR_FUNCTION_KEY>"
+  AZURE_FUNCTION_URL: "https://line-func-app.azurewebsites.net/awardPoints?code=_Ds88BbS1YEKV0BGBRfjwcko_yu83JJ_R-eR1MPV05VbAzFuMDkzEg=="
 };
