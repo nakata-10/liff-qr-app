d411f73d47b5773bcef8c2ed7482dc88e3d6daa0
nakata-10 <tsugumi2528@gmail.com>
2025-06-18 19:17:10 +0900

    Updates

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 window.APP_CONFIG = {
   LIFF_ID: "2007532568-4ggdRvjk",
   SCAN_BASE_URL: 'https://nakata-10.github.io/liff-qr-app',
-  AZURE_FUNCTION_URL: "https://line-func-app.azurewebsites.net/awardPoints",
+  AZURE_FUNCTION_URL: "https://line-func-app.azurewebsites.net/awardpoints",
   FUNCTION_KEY: "_Ds88BbS1YEKV0BGBRfjwcko_yu83JJ_R-eR1MPV05VbAzFuMDkzEg=="
 };
