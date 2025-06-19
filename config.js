3046b5ae5c3ea45349cf7262d756eaf5a6cbc531
chokora1028 <masayamasato1028@gmail.com>
2025-06-19 16:03:34 +0900

    copy整理

--- config.js
+++
@@ -1,7 +0,0 @@
-// public/config.js
-window.APP_CONFIG = {
-  LIFF_ID: "2007532568-4ggdRvjk",
-  SCAN_BASE_URL: 'https://nakata-10.github.io/liff-qr-app',
-  AZURE_FUNCTION_URL: "https://line-func-app.azurewebsites.net/awardPoints",
-  FUNCTION_KEY: "_Ds88BbS1YEKV0BGBRfjwcko_yu83JJ_R-eR1MPV05VbAzFuMDkzEg=="
-};
