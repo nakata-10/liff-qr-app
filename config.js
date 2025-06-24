2ad4d1baec3283dd0bbf5ec32aea0fde1b24b91e
nakata-10 <tsugumi2528@gmail.com>
2025-06-24 10:40:13 +0900

    Updates

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ window.APP_CONFIG = {
   SCAN_BASE_URL: 'https://nakata-10.github.io/liff-qr-app',
   AZURE_FUNCTION_URL: "https://line-func-app.azurewebsites.net/awardPoints",
   SCAN_RESULT_URL: "https://line-func-app.azurewebsites.net/getScanResult",
+  NEGOTIATE_URL: "https://line-func-app.azurewebsites.net/negotiate",
   FUNCTION_KEY: "_Ds88BbS1YEKV0BGBRfjwcko_yu83JJ_R-eR1MPV05VbAzFuMDkzEg=="
 };
